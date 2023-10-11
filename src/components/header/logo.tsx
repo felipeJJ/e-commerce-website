817bc86d69646b47fc98e7a480607e16870b97d2
felipeJJ <jorgefelipe11@hotmail.com>
2023-10-11 20:11:56 -0300

    fix: add href to the logo

--- src/components/header/logo.tsx
+++ src/components/header/logo.tsx
@@ -7,9 +7,10 @@ const sairaStencil = Saira_Stencil_One({
   })
 
 export default function Logo() {
+
     return(
         <div className="h-full w-[544px]">
-            <a className={`${sairaStencil.variable} font-sans text-5xl leading-8 tracking-widest text-gray-700`}>store</a>
+            <a href='/' className={`${sairaStencil.variable} font-sans text-5xl leading-8 tracking-widest text-gray-700`}>store</a>
         </div>
     )
 }
