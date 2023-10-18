f282c7a3bbbc4514c4b6020e3d6ecb505e136cd8
felipeJJ <jorgefelipe11@hotmail.com>
2023-10-18 12:10:07 -0300

    fix: sarchbar

--- src/components/header/sarchBar.tsx
+++ src/components/header/sarchBar.tsx
@@ -4,7 +4,7 @@ import SarchIcon from './sarchIcon'
 export default function SarchBar() {
     return(
         <div className=" bg-gray-100 w-[359px] h-11 py-2 px-4 rounded-lg flex mr-5">
-            <input type="text" className=" outline-none bg-gray-100 w-[206px] mr-24" placeholder='Procura por algo específico'/>
+            <input type="text" className=" outline-none bg-gray-100 w-[296px] pr-20" placeholder='Procura por algo específico'/>
             <SarchIcon/>
         </div>
 )
