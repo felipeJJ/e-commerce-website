2849603dd338d12f41f9eabfb6e3ce5488c76f8b
felipeJJ <jorgefelipe11@hotmail.com>
2023-11-08 00:56:31 -0300

    refactor: change how the setCount works

--- src/components/header/cartController.tsx
+++ src/components/header/cartController.tsx
@@ -5,11 +5,10 @@ export default function CartControler() {
 const { count, setCount } = useCartContext()
   
   useEffect(() => {
-    let TemCount = Number(localStorage.getItem('count')) 
-    if (TemCount) setCount(TemCount)  
+    const cartItems = JSON.parse(localStorage.getItem('cart-items') || '[]')
+      setCount(cartItems.length) 
   }, [setCount])
   
-
   return (
     <div className="bg-red-600 w-4 h-4 rounded-full translate-y-3 translate-x-4 absolute flex justify-center">
       {count >= 0 && (
