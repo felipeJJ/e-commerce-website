6ed8adebe5cbc98db38b4144ec982121f06b4798
felipeJJ <jorgefelipe11@hotmail.com>
2023-11-26 02:44:33 -0300

    build: add npm depencency

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,7 +4,7 @@ import './globals.css'
 import Header from '@/components/header/header'
 import { FilterContextProvider } from "@/contexts/filterContext"
 import { CartContextProvider } from '@/contexts/cartContext'
-
+import '@sweetalert2/theme-bootstrap-4/bootstrap-4.css'
 
 export default function RootLayout({
   children,
