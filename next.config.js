92d3ad30d725fb540368d21f34e0fb2c50c7cf0e
felipeJJ <jorgefelipe11@hotmail.com>
2024-02-01 19:41:06 -0300

    build: adding a experimental config to tailwind

--- next.config.js
+++ next.config.js
@@ -1,5 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = { 
+  experimental: {
+    appDir: true,
+  },
+  compiler: {
+    styledComponents: true,
+  },
   images: {
     domains: ['jorgim-ecommerce.s3.sa-east-1.amazonaws.com'], 
     remotePatterns: [
