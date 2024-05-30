74652cc053457b1b9f1758ed35cdb9eaa6586572
felipeJJ <jorgefelipe11@hotmail.com>
2024-05-30 20:23:30 -0300

    feat: new mongoose schema for credit cards

---
+++ database/schemas/creditCard.ts
@@ -0,0 +1,16 @@
+import mongoose, {Schema} from "mongoose"
+ 
+const CreditCardSchema = new Schema({
+        cardNumber: {type: String, required: true},
+        cardHolderName: {type: String, required: true},
+        expirationDate: {type: String, required: true},
+        cvc: {type: String, required: true},
+        userId: {type: String, required: true},
+    },
+    {
+        timestamps: true,
+    }
+)
+
+const CreditCard = mongoose.models.CreditCard || mongoose.model("CreditCard", CreditCardSchema)
+export default CreditCard
