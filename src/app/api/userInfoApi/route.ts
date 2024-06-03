2fb7ed9525cb7489ec2d79b5eae6cc560369f724
felipeJJ <jorgefelipe11@hotmail.com>
2024-06-03 15:27:06 -0300

    feat: improving security with session check

--- src/app/api/userInfoApi/route.ts
+++ src/app/api/userInfoApi/route.ts
@@ -7,36 +7,41 @@ import { getServerSession } from "next-auth"
 import { handler } from "../auth/[...nextauth]/route"
 
 export async function POST(req: Request) {
-    await database.connectMongo()
-    try {
-        const { name, cpf, cellphone, email, password, confirmPassword,
-            state, city, zip, address, houseNumber, district
-        } = await req.json() as unknown  as UserInfoData
+    const session = await getServerSession(handler)
 
-        const emailExists = await UserInfo.findOne({ email })
-        if (emailExists) {
+    try {
+        if(session){
+            await database.connectMongo()
+
+            const { name, cpf, cellphone, email, password, confirmPassword,
+                state, city, zip, address, houseNumber, district
+            } = await req.json() as unknown  as UserInfoData
+    
+            const emailExists = await UserInfo.findOne({ email })
+            if (emailExists) {
+                return NextResponse.json(
+                    { message: "Email já existe" },
+                    { status: 409 },
+                )
+            }
+    
+            let userPayload: Partial<UserInfoData> = {
+                name, cpf, cellphone, email,
+                state, city, zip, address, houseNumber, district
+            }
+    
+            if (password) {
+                const hashedPassword = await bcrypt.hash(password, 5)
+                userPayload = { ...userPayload, password: hashedPassword, confirmPassword: hashedPassword }
+            }
+    
+            await UserInfo.create(userPayload)
+            
             return NextResponse.json(
-                { message: "Email já existe" },
-                { status: 409 },
+                { message: "Usuário criado com susseco!", UserInfo },
+                { status: 200 }
             )
         }
-
-        let userPayload: Partial<UserInfoData> = {
-            name, cpf, cellphone, email,
-            state, city, zip, address, houseNumber, district
-        }
-
-        if (password) {
-            const hashedPassword = await bcrypt.hash(password, 5)
-            userPayload = { ...userPayload, password: hashedPassword, confirmPassword: hashedPassword }
-        }
-
-        await UserInfo.create(userPayload)
-        
-        return NextResponse.json(
-            { message: "Usuário criado com susseco!", UserInfo },
-            { status: 200 }
-        )
     } catch (error) {
         return NextResponse.json(
             { message: "Error ao salvar cadstro de usuário", error },
@@ -46,15 +51,14 @@ export async function POST(req: Request) {
 }
 
 export async function GET(req: Request) {
-    await database.connectMongo()
-
     const session = await getServerSession(handler)
-
+    
     const url = new URL(req.url)
     const email = url.searchParams.get('email')
-
+    
     try {
         if(session?.user?.email === email){
+            await database.connectMongo()
 
             const user: UserInfoData | null = await UserInfo.findOne({ email }).lean()
             if (user) {
@@ -78,28 +82,32 @@ export async function GET(req: Request) {
 }
 
 export async function PUT(req: Request) {
-    await database.connectMongo()
-
+    const session = await getServerSession(handler)
+    
     try {
-        const { email, ...addressData } = await req.json() as { email: string } & UserAddress
+        if(session){
+            await database.connectMongo()
 
-        const user = await UserInfo.findOneAndUpdate(
-            { email },
-            { $set: addressData },
-            { new: true }
-        )
-
-        if (user) {
-            return NextResponse.json(
-                { message: "Endereço atualizado com sucesso", user },
-                { status: 200 }
+            const { email, ...addressData } = await req.json() as { email: string } & UserAddress
+            const user = await UserInfo.findOneAndUpdate(
+                { email },
+                { $set: addressData },
+                { new: true }
             )
-        } else {
-            return NextResponse.json(
-                { message: "Usuário não encontrado" },
-                { status: 404 }
-            )
+    
+            if (user) {
+                return NextResponse.json(
+                    { message: "Endereço atualizado com sucesso", user },
+                    { status: 200 }
+                )
+            } else {
+                return NextResponse.json(
+                    { message: "Usuário não encontrado" },
+                    { status: 404 }
+                )
+            }
         }
+
     } catch (error) {
         return NextResponse.json(
             { message: "Erro ao atualizar endereço", error },
