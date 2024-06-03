7caa1efb504876172282d10d1b56f9f60818715a
felipeJJ <jorgefelipe11@hotmail.com>
2024-06-03 01:43:20 -0300

    feat: new feature to encrypt card data

--- src/app/api/newCreditCardApi/route.ts
+++ src/app/api/newCreditCardApi/route.ts
@@ -1,31 +1,67 @@
+import crypto from "crypto"
+import dotenv from "dotenv"
 import { NextResponse } from "next/server"
-import bcrypt from "bcryptjs"
+import { getServerSession } from "next-auth"
 import { creditCardData } from "../../../../types"
+import { handler } from "../auth/[...nextauth]/route"
 import database from "../../../../database/lib/mongoose"
 import CreditCard from "../../../../database/schemas/creditCard"
 
-export async function POST(req: Request, res: Response) {
-    await database.connectMongo()
-    
+dotenv.config()
+
+function encrypt(text: string, iv: Buffer): string {
+    if (!process.env.CRYPTO_SECRET_KEY) {
+        throw new Error('Variável de ambiente CRYPTO_SECRET_KEY não está definida.')
+    }
+    const key = Buffer.from(process.env.CRYPTO_SECRET_KEY)
+    let cipher = crypto.createCipheriv("aes-256-cbc", key, iv)
+    let encrypted = cipher.update(text, "utf8", "hex")
+    encrypted += cipher.final("hex")
+    return encrypted
+}
+
+export async function POST(req: Request, res: Response) { 
+    const session = await getServerSession(handler)
+
     try {
-        const { cardNumber, cardHolderName, expirationDate, cvc, userId } = await req.json()
+        if(session){
+            await database.connectMongo()
 
-        let cadrPayload: creditCardData = { cardNumber, cardHolderName, expirationDate, cvc, userId }
+            const { cardNumber, cardHolderName, expirationDate, cvc, userId } = await req.json()
 
-        const hashedCardNumber = await bcrypt.hash(cardNumber, 5)
-        const hashedCardHolderName = await bcrypt.hash(cardHolderName, 5)
-        
-        cadrPayload = { ...cadrPayload, cardNumber: hashedCardNumber, cardHolderName: hashedCardHolderName}
-        
-        await CreditCard.create(cadrPayload)
+            const existingCard = await CreditCard.findOne({ userId, cardNumber })
+            if (existingCard) {
+                return NextResponse.json(
+                    { message: "Já existe um cartão com este número para o usuário informado." },
+                    { status: 400 }
+                )
+            }
 
-        return NextResponse.json(
-            { message: "Criado com sucesso", CreditCard },
-            { status: 200 }
-        )
+            const iv = crypto.randomBytes(16)
+            const ivHex = iv.toString('hex')
+    
+            const encryptedCardNumber = encrypt(cardNumber, iv)
+            const encryptedCardHolderName = encrypt(cardHolderName, iv)
+            
+            const cardPayload: creditCardData = {
+                cardNumber: encryptedCardNumber, 
+                cardHolderName: encryptedCardHolderName,
+                expirationDate,
+                cvc,
+                userId,
+                iv: ivHex,
+            }
+            
+            await CreditCard.create(cardPayload)
+            return NextResponse.json(
+                { message: "Criado com sucesso", CreditCard },
+                { status: 200 }
+            )
+        }
     } catch (error: unknown) {
+        console.error(error)
         return NextResponse.json(
-            { message: "Erro ao salvar cartão de créidito", error },
+            { message: "Erro ao salvar cartão de crédito", error: error instanceof Error? error.message : "Erro desconhecido" },
             { status: 500 }
         )
     }
