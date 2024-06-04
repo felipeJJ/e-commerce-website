39ecd721f1a245413c95cd537ef7ea17ad064da3
felipeJJ <jorgefelipe11@hotmail.com>
2024-06-04 18:33:14 -0300

    feat: adding feature to get the selected credit card from database and use this data

--- src/app/api/cieloApi/route.ts
+++ src/app/api/cieloApi/route.ts
@@ -1,9 +1,16 @@
+import crypto from "crypto"
+import dotenv from "dotenv"
 import axios from "axios"
 import generateUniqueId from "generate-unique-id"
 import { NextResponse } from "next/server"
+import database from "../../../../database/lib/mongoose"
+import CreditCard from "../../../../database/schemas/creditCard"
+import { creditCardData } from "../../../../types"
+
+dotenv.config()
 
 export async function POST(req: Request, res: Response) {
-    const { cardNumber, cardHolderName, expirationDate, cvc, amount } = await req.json()
+    const { userId, cardId ,amount } = await req.json()
 
     const orderId = generateUniqueId({
         length: 10,
@@ -12,6 +19,8 @@ export async function POST(req: Request, res: Response) {
     })
 
     try {
+        const cardData = await getCardData(userId, cardId)
+
         const response = await axios.post(
             "https://apisandbox.cieloecommerce.cielo.com.br/1/sales",
             {
@@ -22,10 +31,10 @@ export async function POST(req: Request, res: Response) {
                     "Installments": 1,
                     "SoftDescriptor": "123456789ABCD",
                     "CreditCard": {
-                        "CardNumber": cardNumber,
-                        "Holder": cardHolderName,
-                        "ExpirationDate": expirationDate,
-                        "SecurityCode": cvc,
+                        "CardNumber": cardData.cardNumber,
+                        "Holder": cardData.cardHolderName,
+                        "ExpirationDate": cardData.expirationDate,
+                        "SecurityCode": cardData.cvc,
                         "Brand": "Visa"
                     }
                 }
@@ -61,5 +70,36 @@ export async function POST(req: Request, res: Response) {
             { message: "Erro ao solicitar pagamento", error: errorMessage },
             { status: 500 }
         )
+    }
+}
+
+function decrypt(encryptedText: string, iv: Buffer): string {
+    if (!process.env.CRYPTO_SECRET_KEY) {
+        throw new Error('Variável de ambiente CRYPTO_SECRET_KEY não está definida.')
+    }
+    const key = Buffer.from(process.env.CRYPTO_SECRET_KEY)
+    const decipher = crypto.createDecipheriv('aes-256-cbc', key, iv)
+    let decrypted = decipher.update(encryptedText, 'hex', 'utf8')
+    decrypted += decipher.final('utf8')
+    return decrypted
+}
+
+export async function getCardData(userId: string, cardId: string): Promise<creditCardData> {
+    await database.connectMongo()
+
+    const creditCard = await CreditCard.findOne({ _id: cardId, userId })
+    if (!creditCard) {
+        throw new Error('Cartão de crédito não encontrado.')
+    }
+
+    const ivBuffer = Buffer.from(creditCard.iv, 'hex')
+    const decryptedCardNumber = decrypt(creditCard.cardNumber, ivBuffer)
+    const decryptedCardHolderName = decrypt(creditCard.cardHolderName, ivBuffer)
+
+    return {
+        cardNumber: decryptedCardNumber,
+        expirationDate: creditCard.expirationDate,
+        cvc: creditCard.cvc,
+        cardHolderName: decryptedCardHolderName,
     }
 }
