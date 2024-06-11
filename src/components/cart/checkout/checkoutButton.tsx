cd96d1f721e22c0a3b7c8da3937378338d0dc679
felipeJJ <jorgefelipe11@hotmail.com>
2024-06-11 19:25:19 -0300

    feat: feature to change the button name, function to be executed when the button is pressed

--- src/components/cart/checkout/checkoutButton.tsx
+++ src/components/cart/checkout/checkoutButton.tsx
@@ -1,18 +1,23 @@
 'use client'
 
+import { useCheckoutContext } from "@/contexts/checkoutContext"
 import { useCartContext } from "@/contexts/cartContext"
+import { usePathname, useRouter } from "next/navigation"
+import withReactContent from "sweetalert2-react-content"
 import { useSession } from "next-auth/react"
-import { useRouter } from "next/navigation"
-import withReactContent from 'sweetalert2-react-content'
-import Swal from 'sweetalert2'
+import Swal from "sweetalert2"
 import axios from "axios"
 
 export default function CheckoutButton(){
     const { itemsCount } = useCartContext()
+    const { amount, selectedCardId } = useCheckoutContext()
     const {data: session} = useSession()
     const router = useRouter()
-
+    const pathName = usePathname()
+    
     const MySwal = withReactContent(Swal)
+    
+    let buttonName = "CONTINUAR"
     
     function verifyEmailAcount(email: string | null | undefined){
         axios.get(`/api/userInfoApi?email=${email}` ).then(response => {
@@ -37,8 +42,39 @@ export default function CheckoutButton(){
         })
     }
 
+    const getUserIdFromSessionStorage = () => {
+        const userData = sessionStorage.getItem('userData')
+        if (userData) {
+            try {
+                const parsedUserData = JSON.parse(userData)
+                return parsedUserData._id
+            } catch (error) {
+                console.error("Erro ao analisar userData do sessionStorage:", error)
+                return null
+            }
+        }
+        return null
+    }
+
+    async function handleSubmit(){
+        const userId = getUserIdFromSessionStorage()
+        const cardId = selectedCardId
+
+        try {
+            const response = await axios.post("/api/cieloApi", { userId, amount, cardId})
+            console.log(response.data)
+            // Trate a resposta conforme necessário
+        } catch (error) {
+            console.error("Erro ao finalizar a compra:", error)
+            // Trate o erro conforme necessário
+        }   
+    }
+
     function handleCheckout() {
         if(session){
+            if(pathName === "/checkout"){
+                handleSubmit()
+            }
             verifyEmailAcount(session.user?.email)
         } else {
             MySwal.fire({
@@ -53,12 +89,16 @@ export default function CheckoutButton(){
         }
     }
 
+    if(pathName === '/checkout'){
+        buttonName = "FINALIZAR"
+    }
+
     return(
         <button 
             disabled={!itemsCount}
             onClick={handleCheckout}
             className="w-[304px] h-11 mt-10 bg-[#51B853] text-white rounded-lg">
-            CONTINUAR
+            {buttonName}
         </button>
     )
 }
